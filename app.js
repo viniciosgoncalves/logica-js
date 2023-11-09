ba4e7e518ae3857eb08d9dd345110e4f962d6ba8
Vinicios Jose Gonçalves de Jesus <vinicios.goncalves@compasso.com.br>
2023-11-09 14:49:13 -0300

    exemplos aula 4 jogo numero secreto com random, condições, laço while, encrementador, operador ternario

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 29;
+//numeroSecreto gerado aleatoriamente até 100
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 console.log(numeroSecreto);
 let numero;
 let tentativas = 1;
 
 // While enquanto chute não for igual o numero secreto
 while (numero != numeroSecreto) {
-    numero = prompt('Escolha um número entre 1 e 30');
+    numero = prompt('Escolha um número entre 1 e 100');
     // se chute for igual ao numero secreto
     if (numero == numeroSecreto) {
-        alert(`Isso ai! Você descobiu o número secreto! ${numeroSecreto} com ${tentativas} tentativas`);
+        //PARA aqui se acertar de primeira
+        break;
     } else {
         if (numero > numeroSecreto) {
             alert(`O número secreto é menor que ${numero}`);
@@ -20,4 +22,16 @@ while (numero != numeroSecreto) {
         tentativas++;
     }
 }
+//continua aqui caso tenha acertado de primeira para sair fora do while, se não segue o else comentado abaixo
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Você descobiu o número secreto! ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+/* DA PRA FAZER DESSA FORMA TAMBEM ENTRETANTO A FORMA ACIMA É MELHOR
+if (tentativas > 1){
+    alert(`Isso ai! Você descobiu o número secreto! ${numeroSecreto} com ${tentativas} tentativas`);
+} else {
+    alert(`Isso ai! Você descobiu o número secreto! ${numeroSecreto} com ${tentativas} tentativa`);
+}
+*/
+
 

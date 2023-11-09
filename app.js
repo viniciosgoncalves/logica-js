8e1f24ca5ebd7630029602f2da74de9e907ff7d4
Vinicios Jose Gonçalves de Jesus <vinicios.goncalves@compasso.com.br>
2023-11-09 08:29:36 -0300

    exemplos aula 3 novas condições laço while

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 29;
-console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 30');
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
-if (chute == numeroSecreto) {
-    alert (`Isso ai! Você descobiu o número secreto! ${numeroSecreto}`);
-} else {
-    alert ('Você errou :(');
+// While enquanto chute não for igual o numero secreto
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 30');
+    // se chute for igual ao numero secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso ai! Você descobiu o número secreto! ${numeroSecreto} com ${tentativas} tentativas`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        tentativas++;
+    }
 }
+

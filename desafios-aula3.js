18a640bf26b52b6fd807b4987ae8c892059ca2d9
Vinicios Jose Gonçalves de Jesus <vinicios.goncalves@compasso.com.br>
2023-11-09 09:51:53 -0300

    desafios do curso

---
+++ desafios-aula3.js
@@ -0,0 +1,36 @@
+// 1 Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
+
+// let contador = 1;
+// while (contador <= 10) {
+//     console.log(contador);
+//     contador++;
+// }
+
+// 2 Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
+
+// let contador2 = 10;
+// while (contador2 >= 0) {
+//     console.log(contador2);
+//     contador2--;
+// }
+
+/* 3 Crie um programa de contagem regressiva. Peça um número e conte deste número até 0,
+usando um loop while no console do navegador.*/
+
+let numeroMaximo = prompt("Digite um número para a contagem regressiva:");
+
+while (numeroMaximo >= 0) {
+    console.log(numeroMaximo);
+    numeroMaximo--;
+}
+
+/* 4 Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número,
+usando um loop while no console do navegador.*/
+
+let numeroMaximo2 = prompt("Digite um número para a contagem progregressiva:");;
+let contador = 0;
+
+while (contador <= numeroMaximo23) {
+    console.log(contador);
+    contador++
+}
